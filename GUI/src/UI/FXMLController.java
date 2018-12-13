ae5e0a0ac1c7bcd5073fe832a78d3b7e8afdaa7a
Justin <taiwanjustinliu@gmail.com>
2018-12-13 19:57:17 +0000

    arm window in joystick interface and more buttons, infinite arm windows can be opened atm.

--- GUI/src/UI/FXMLController.java
+++ GUI/src/UI/FXMLController.java
@@ -5,14 +5,20 @@
  */
 package UI;
 
+import java.io.IOException;
 import static java.lang.Math.atan;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
 import javafx.scene.input.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -29,6 +35,7 @@ public class FXMLController implements Initializable {
     @FXML private Stage stage;
     @FXML private Text DispJoyX;
     @FXML private Text DispJoyY;
+    @FXML private Circle buttonArm;
     
     public double deltx = 0;
     public double delty = 0; //location relative to center
@@ -81,9 +88,50 @@ public class FXMLController implements Initializable {
         DispJoyX.setText(dispx);
         DispJoyY.setText(dispy);
     }
-//    public void handleConnectButtonPress(ActionEvent e) {
-//        System.out.println("Hello");
-//    }
+    public void openArm(MouseEvent e){
+//        System.out.println("Opening Arm Window");
+//        try {
+//        FXMLLoader fxmlLoader = new FXMLLoader();
+//        fxmlLoader.setLocation(getClass().getResource("Arm.fxml"));
+//        /* 
+//         * if "fx:controller" is not set in fxml
+//         * fxmlLoader.setController(NewWindowController);
+//         */
+//        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+//        Stage armStage = new Stage();
+//        armStage.setTitle("Your arms");
+//        armStage.setResizable(false);
+//        armStage.setScene(scene);
+//        armStage.show();
+//        } catch (IOException a) {
+//            Logger logger = Logger.getLogger(getClass().getName());
+//            logger.log(Level.SEVERE, "Failed to create new Window.", a);
+//        }
+        try {
+            final FXMLLoader loader = new FXMLLoader(getClass().getResource("Arm.fxml"));
+            final Parent root = (Parent) loader.load();
+            Stage armStage = new Stage();
+            root.getStylesheets().add("UI/style.css");
+            //root.getChildren().add(btn);
+
+            Scene scene = new Scene(root, 500, 500);
+
+            armStage.setTitle("Rover");
+            armStage.setScene(scene);
+            armStage.setResizable(false);
+            armStage.show();
+//            //ResourceBundle resources = null;
+//            root = FXMLLoader.load(getClass().getClassLoader().getResource("Arm.fxml"));
+//            root.getStylesheets().add("UI/style.css");
+//            Stage armStage = new Stage();
+//            armStage.setTitle("Arm!");
+//            armStage.setScene(new Scene(root, 500, 500));
+//            armStage.setResizable(false);
+//            armStage.show();
+        }
+        catch (IOException a) {
+        }
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
