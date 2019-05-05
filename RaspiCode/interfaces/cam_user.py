506824f07ff81f509b1e4722dda689ee3b10588f
ranulpallemulle <ranulp96@gmail.com>
2019-05-05 05:22:45 +0100

    Fixed lack of avfvideosrc (mac) support

--- RaspiCode/interfaces/cam_user.py
+++ RaspiCode/interfaces/cam_user.py
@@ -72,6 +72,8 @@ class CameraUser:
             tune = ' '
         elif device == 'v4l2src':
             compressor = 'x264enc'
+        elif device == 'avfvideosrc':
+            compressor = 'x264enc'            
             tune = ' tune=zerolatency '
         elif device == 'avfvideosrc':
             compressor = 'x264enc'            
