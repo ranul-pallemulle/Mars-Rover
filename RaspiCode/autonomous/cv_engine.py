55f7e4aa4d153d7d5789ea323bf2c1ba1091de70
ranulpallemulle <ranulp96@gmail.com>
2019-06-13 14:26:56 +0100

    Put CVEngine stuff into separate file.
    OpenCVHaar now uses camera.get_frame instead of self.get_camera_frame.

---
+++ RaspiCode/autonomous/cv_engine.py
@@ -0,0 +1,64 @@
+from abc import ABC, abstractmethod
+from coreutils.diagnostics import Diagnostics as dg
+from interfaces.cam_user import CameraUser, CameraUserError
+from threading import Lock
+import cv2
+import time
+
+class CVEngine(ABC, CameraUser):
+    '''Computer vision engine: has object recognition functionality.'''
+    def __init__(self):
+        CameraUser.__init__(self)
+        self.active = False
+        self.active_lock = Lock()
+
+    def activate(self):
+        self.acquire_camera()        
+        self.initialise()
+        with self.active_lock:
+            self.active = True
+
+    def deactivate(self):
+        with self.active_lock:
+            self.active = False
+        time.sleep(1)
+        if self.have_camera():
+            self.release_camera()
+
+    def is_active(self):
+        with self.active_lock:
+            return self.active
+
+    @abstractmethod
+    def initialise(self):
+        '''Any initialisation (e.g loading a haarcascades file).'''
+        pass
+    
+    @abstractmethod
+    def find_obj(self):
+        '''Return a bounding box (coordinates) of the object within the
+current camera fram.'''
+        pass
+
+
+class OpenCVHaar(CVEngine):
+
+    def initialise(self):
+        self.cascade = cv2.CascadeClassifier('autonomous/haarcascade_frontalface_default.xml')
+        for i in range(5):
+            self.camera.get_frame() # read a few frames
+        
+    def find_obj(self):
+        # try:
+        frame = self.camera.get_frame()
+        # except CameraUserError as e:
+        #     dg.print(str(e))
+        #     self.deactivate()
+        #     return
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        objects = []
+        try:
+            objects = self.cascade.detectMultiScale(gray)
+        except Exception as e:
+            dg.print(str(e))
+        return objects
--- RaspiCode/autonomous/goal_samples.py
+++ RaspiCode/autonomous/goal_samples.py
@@ -1,70 +1,11 @@
-from abc import ABC, abstractmethod
 import coreutils.configure as cfg
 from autonomous.auto_mode import Goal, GoalError
-from coreutils.diagnostics import Diagnostics as dg
-from interfaces.cam_user import CameraUser, CameraUserError
+from autonomous.cv_engine import OpenCVHaar
 from interfaces.actuator import Actuator, ActuatorError
-from threading import Thread, Lock
+from threading import Thread
 import time
 import cv2
 
-class CVEngine(ABC, CameraUser):
-    '''Computer vision engine: has object recognition functionality.'''
-    def __init__(self):
-        CameraUser.__init__(self)
-        self.active = False
-        self.active_lock = Lock()
-
-    def activate(self):
-        self.initialise()
-        self.acquire_camera()
-        with self.active_lock:
-            self.active = True
-
-    def deactivate(self):
-        with self.active_lock:
-            self.active = False
-        time.sleep(1)
-        if self.have_camera():
-            self.release_camera()
-
-    def is_active(self):
-        with self.active_lock:
-            return self.active
-
-    @abstractmethod
-    def initialise(self):
-        '''Any initialisation (e.g loading a haarcascades file).'''
-        pass
-    
-    @abstractmethod
-    def find_obj(self):
-        '''Return a bounding box (coordinates) of the object within the
-current camera fram.'''
-        pass
-
-
-class OpenCVHaar(CVEngine):
-
-    def initialise(self):
-        self.cascade = cv2.CascadeClassifier('autonomous/haarcascade_frontalface_default.xml')
-            
-    def find_obj(self):
-        try:
-            frame = self.get_camera_frame()
-        except CameraUserError as e:
-            dg.print(str(e))
-            self.deactivate()
-            return
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        objects = []
-        try:
-            objects = self.cascade.detectMultiScale(gray)
-        except Exception as e:
-            dg.print(str(e))
-        return objects
-
-        
 class Samples(Goal, Actuator):
     '''Autonomous mode goal for picking up samples.'''
     
