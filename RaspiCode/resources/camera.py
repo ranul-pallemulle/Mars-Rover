d7a1b8a14fb648f190718fdcdbf6348e6c120d6e
ranulpallemulle <ranulp96@gmail.com>
2019-06-13 11:12:06 +0100

    Added doc-strings and removed unnecessary states from camera.py.

--- RaspiCode/resources/camera.py
+++ RaspiCode/resources/camera.py
@@ -8,55 +8,70 @@ class CameraError(Exception):
     pass
 
 class Camera(Resource):
+    '''Provides access to the camera or webcam on the device. Operational modes
+     can access this by inheriting from the CameraUser interface.
+     Camera access implementation uses OpenCV in combination with gstreamer.'''
     def __init__(self):
+        '''Register name and access policy. Obtain settings from 
+        CameraConfiguration. Initialise members.'''
         Resource.__init__(self)
         self.policy = Policy.SHARED
         self.register_name("Camera")
         try:
-            self.source = cfg.cam_config.device()
+            self.source = cfg.cam_config.device() # gstreamer device name
             self.framerate = cfg.cam_config.capture_framerate()
             self.frame_width=cfg.cam_config.capture_frame_width()
             self.frame_height=cfg.cam_config.capture_frame_height()
         except cfg.ConfigurationError as e:
             raise ResourceRawError(str(e))
-        self.gst_comm = None
-        self.cap = None
+        self.gst_comm = None    # string corresponding to gstreamer pipe
+        self.cap = None         # opencv capture object
         self.active = False
-        self.camlock = RWLock()
-        self.frame = None
+        self.camlock = RWLock() # allow multiple readers, single writer
+        self.frame = None       # current camera frame
 
     def shared_init(self):
-       self.start_capture()
+        '''Called by resource manager when camera is acquired for the first
+        time.
+        '''
+        self.start_capture()
 
     def shared_deinit(self):
+        '''Called by resource manager when the last user releases the camera.'''
         self.stop_capture()
 
     def _capture(self):
+        '''Run in a separate thread. While camera is active, store the current
+        camera frame.'''
         while self.active:
             ret,img = self.cap.read()
-            if ret:
+            if ret:             # capture was successful
                 self.camlock.acquire_write()
                 self.frame = img
                 self.camlock.release()
 
     def start_capture(self):
-        if self.cap is None and not self.active:
+        '''Begin camera operation, if it is not already active.'''
+        if not self.active:
             self._eval_gst_comm()
-            self.cap = cv2.VideoCapture(self.gst_comm)
-            self.active = True
+            self.cap = cv2.VideoCapture(self.gst_comm) # get camera access
+            self.active = True  # Allow _capture loop to start
             Thread(target=self._capture, args=()).start()
         else:
-            raise CameraError('Camera already initialised: release before reinitialising.')
+            raise CameraError('Camera already initialised: release before \
+reinitialising.')
 
     def stop_capture(self):
-        if self.cap is not None and self.active:
-            self.active = False
-            self.cap.release()
-            self.cap = None
+        '''End camera operation.'''
+        if self.active:
+            self.active = False # Allow _capture loop to end
+            self.cap.release()  # release camera from program
         else:
-            raise CameraError('Camera already released: cannot release.')
+            raise CameraError('Camera already inactive: cannot deactivate.')
 
     def get_frame(self):
+        '''If camera is active, return (a deep copy of) the current frame. Else,
+        throw an exception.'''
         if self.active:
             self.camlock.acquire_read()
             frame = self.frame.copy()
@@ -65,29 +80,12 @@ class Camera(Resource):
         raise CameraError('Camera not active: cannot get frame.')
             
     def _eval_gst_comm(self):
-        self.gst_comm = self.source+' ! video/x-raw,framerate='+str(self.framerate)+'/1,width='+str(self.frame_width)+',height='+str(self.frame_height)+' ! videoconvert ! appsink name=opencvsink sync=false'
+        '''Evaluate a gstreamer pipeline string to initialise the OpenCV 
+        VideoCapture object with.'''
+        self.gst_comm = self.source+' ! video/x-raw,framerate='+ \
+            str(self.framerate)+'/1,width='+str(self.frame_width)+',height='+ \
+            str(self.frame_height)+ \
+            ' ! videoconvert ! appsink name=opencvsink sync=false'
 
     def is_active(self):
         return self.active
-
-    def get_framerate(self):
-        return self.framerate
-
-    def get_frame_height(self):
-        return self.frame_height
-
-    def get_frame_width(self):
-        return self.frame_width
-
-    def set_framerate(self, framerate):
-        self.framerate = framerate
-        self._eval_gst_comm
-
-    def set_frame_height(self, frame_height):
-        self.frame_height = frame_height
-        self._eval_gst_comm
-
-    def set_frame_width(self, frame_width):
-        self.frame_width = frame_width
-        self._eval_gst_comm
-
