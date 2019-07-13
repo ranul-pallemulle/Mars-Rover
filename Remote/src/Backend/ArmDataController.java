ee1a30ea74b976eafa2a4419321747e7c6c50ac6
Ranul Pallemulle <ranulp96@gmail.com>
2019-07-14 02:33:53 +0530

    Exception handling complete for now in ArmDataController.

--- Remote/src/Backend/ArmDataController.java
+++ Remote/src/Backend/ArmDataController.java
@@ -6,6 +6,7 @@
 package Backend;
 
 import Exceptions.FormatException;
+import Exceptions.NotFoundException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -15,70 +16,80 @@ import java.io.IOException;
 import static java.lang.Math.PI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- *
- * @author ranul
+ * Control reading and writing of robotic arm servo angle settings to/from a 
+ * data file.
+ * @author Ranul Pallemulle
  */
 public class ArmDataController {
     
-    private File file;
+    private File file; // data file containing angle settings
     private BufferedReader bReader;
     private BufferedWriter bWriter;
-    private List<String> lines;
+    private List<String> lines; // named angle settings read from data file
     
     public ArmDataController () {
         lines = new ArrayList<>();
     }
     
+    /**
+     * Attempt to open a data file. If a file was previously open, close it.
+     * @param _file
+     * @throws IOException - if file cannot be opened for read and write
+     * @throws FormatException - if the file is of the wrong format
+     */
     public void importFile(File _file) throws IOException, FormatException {
         if (file != null) {
-            closeFile();
+            closeFile(); // close previously open file
         }
         file = _file;
         bReader = new BufferedReader(new FileReader(file));
         bWriter = new BufferedWriter(new FileWriter(file, true)); // open in append mode
         // Read data in to the member list
+        lines.clear(); // discard previous data, if present
         String line;
-        List<String> backupList = new ArrayList<>(lines); // restore later if needed
-        lines.clear();
         while ((line = bReader.readLine()) != null) {
-            lines.add(line);
+            lines.add(line); // read data in to member list
         }
-        // Check that new data is of the right format
+        // Check that new data is valid
         String error;
         if ((error = invalidData(lines)) != null) {
-            lines = backupList;
             throw new FormatException (error);
         }
         // Check that the new data contains settings for DROP1, DROP2, WATCH and PICK
         if (!containsData(lines,"DROP1") || !containsData(lines,"DROP2") ||
             !containsData(lines,"WATCH") || !containsData(lines,"PICK")) {
-                lines = backupList;
                 throw new FormatException("Data file does not contain required "
                         + "settings for DROP1,DROP2,WATCH and PICK.");
         }
         
     }
     
-    public void editDataItem(String data) throws IOException {
-        if (file == null) {
-            throw new FileNotOpenException;
+    
+    /**
+     * Change existing data or add new data if it doesn't exist to the member 
+     * list and write out the changes to the data file.
+     * @param data - data string containing data name and angles
+     * @throws IOException - if file is not open
+     * @throws FormatException - if data argument is not of the right format
+     */
+    public void editDataItem(String data) throws IOException, FormatException{
+        if (!isValidDataString(data)) {
+            throw new FormatException("Invalid data format.");
         }
-        int colonIdx = data.indexOf(":");
-        if (colonIdx == -1) {
-            return; // TODO
-        }
-        String dataName = data.substring(0, colonIdx);
+        String dataName = findDataName(data);
         for (int i = 0; i < lines.size(); ++i) { // loop over all lines
             if (lines.get(i).equals(data)) { // data already exists
                 return; // nothing to do
             }
             if (lines.get(i).contains(dataName)) { // different data of same name exists
-                lines.set(i, data); // edit 
-                updateFileWithList();
+                lines.set(i, data); // edit member list
+                updateFileWithList(); // write out changes
                 return;
             }
         }
@@ -90,43 +101,82 @@ public class ArmDataController {
     }
     
     
-    public void removeDataItem(String data) throws IOException {
+    /**
+     * Remove the specified data from the member list and write out the changes 
+     * to the data file.
+     * @param data
+     * @throws IOException - if file is not open
+     * @throws NotFoundException - specified data does not exist on the member 
+     * list to delete it.
+     */
+    public void removeDataItem(String data) throws IOException, NotFoundException {
         int delIdx = lines.indexOf(data);
         if (delIdx == -1) {
-            return;
+            throw new NotFoundException("Data not found.");
         }
-        lines.remove(data);
-        updateFileWithList();
+        lines.remove(data); // edit member list
+        updateFileWithList(); // write out changes
     }
     
-    public void closeFile () throws IOException {
-        bReader.close();
-        bWriter.close();
+    
+    /**
+     * Close the data file. Ignore IOException.
+     */
+    public void closeFile () {
+        try {
+            bReader.close();
+            bWriter.close();
+            file = null;
+        } catch (IOException ex) {
+            Logger.getLogger(ArmDataController.class.getName()).log(Level.WARNING, null, ex);
+        }
     }
     
+    
+    /**
+     * Return list containing all data items.
+     * @return 
+     */
     public List<String> getAllItems() {
         return lines;
     }
     
-    public double[] parseData(String data) {
-        int colonIdx = data.indexOf(":");
-        String subdat = data.substring(colonIdx+1);
-        String[] angstr = subdat.split(",");
-        if (angstr.length != 4) {
-            
+    
+    /**
+     * Extract robotic arm angle data from a data string.
+     * @param data - String containing data name and arm angle data in the order 
+     * base,elbow,top,gripper
+     * @return - array of 4 angles in the order base,elbow,top,gripper
+     * @throws FormatException 
+     */
+    public double[] parseData(String data) throws FormatException {
+        if (!isValidDataString(data)) {
+            throw new FormatException("Invalid data format.");
         }
-        double[] ang = new double[4];
+        String angleData = findAngleString(data);
+        String[] angleStringList = angleData.split(","); // list of angles
+        double[] angles = new double[4];
         for (int i = 0; i < 4; ++i) {
-            ang[i] = -Double.parseDouble(angstr[i])*PI/180;
+            angles[i] = -Double.parseDouble(angleStringList[i])*PI/180;
         }
-        return ang;
+        return angles;
     }
     
-    public boolean haveFile () {
+    
+    /**
+     * Return data file status.
+     * @return 
+     */
+    public boolean dataFileOpen() {
         return (file != null);
     }
     
-    private void updateFileWithList () throws IOException {
+    
+    /**
+     * Write out data as listed in the member list to the data file.
+     * @throws IOException - if the data file is not open
+     */
+    private void updateFileWithList() throws IOException {
         bWriter.close(); // close while in append mode
         bWriter = new BufferedWriter(new FileWriter(file, false)); // reopen in overwrite mode
         if (lines.size() > 0) {
@@ -140,65 +190,96 @@ public class ArmDataController {
             bWriter.write(lines.get(i));
         }
         bWriter.flush();
-        bReader = new BufferedReader(new FileReader(file));
+        bReader = new BufferedReader(new FileReader(file)); // reinitialize reader
     }
+    
     
     /**
-     * Checks a list of Strings for whether a data item of a given name is 
-     * present and returns true if it is. If the List of Strings is of the 
-     * wrong format, throws a FormatException.
-     * @param data
-     * @param name
-     * @return 
+     * Check a list of Strings for whether a data item of a given name is 
+     * present. Assume data is of the correct format and no duplicates are 
+     * present.
+     * @param data - list containing data to match against
+     * @param name - data name to search for
+     * @return - true if data item of the specified name is present, else false.
      */
-    private boolean containsData (List<String> data, String name) throws FormatException {
-        boolean found = false;
+    private boolean containsData (List<String> data, String name) {
         for (String line : data) {
-            int colonIdx = line.indexOf(":");
-            if (colonIdx == -1) {
-                throw new FormatException("Line does not contain colon.");
-            }
-            String dataName = line.substring(0,colonIdx);
+            String dataName = findDataName(line);
             if (dataName.equals(name)) {
-                if (found) {
-                    throw new FormatException("Duplicate data name found.");
-                }
-                found = true;
+                return true;
             }
         }
-        if (found) {
-            return true;
-        }
-        else {
-            return false;
-        }
-        
+        return false;
     }
     
-    private String invalidData(List<String> data) throws FormatException {
-        // regex for pattern like "name:12,172,0,1" or "other:12,-50,-1,2"
+    
+    /**
+     * Check list of dataList items for presence of dataList, validity of format, and 
+     * presence of duplicates.
+     * @param dataList - list of Strings, each of which is to be checked.
+     * @return - an error message if any dataList is invalid. null if all dataList is 
+     * valid.
+     */
+    private String invalidData(List<String> dataList) {
+        String error = null;
+        if (dataList.isEmpty()) {
+            error = "Empty data list.";
+        }
+        List<String> namesList = new ArrayList<>();
+        for (String line : dataList) {
+            if (!isValidDataString(line)) {
+                error = "Line in data has an invalid format.";
+                break;
+            }
+            String dataName = findDataName(line);
+            // regex matches - no need to check if findDataName returns ""
+            if (namesList.contains(dataName)) {
+                error = "Data list contains duplicates";
+                break;
+            }
+            else {
+                namesList.add(dataName);
+            }
+        }
+        return error;
+    }
+    
+    
+    /**
+     * Check if data string is of the right format
+     * @param data - string to check
+     * @return - true if data is of the right format
+     */
+    private boolean isValidDataString(String data) {
         String regex = "\\w+:-?[0-9]{1,3},-?[0-9]{1,3},-?[0-9]{1,3},-?[0-9]{1,3}"; 
         Pattern pattern = Pattern.compile(regex);
-        Matcher m; 
-        for (String line : data) {
-            m = pattern.matcher(line);
-            if (!m.matches()) {
-                return "Line in data has an invalid format";
-            }
-        }
-        return null;
+        Matcher m = pattern.matcher(data);
+        return m.matches();
     }
     
-    private int findOccurrences(String expr, String pattern) {
-        int idx = 0;
-        int count = 0;
-        while (idx != -1) {
-            idx = expr.indexOf(pattern,idx);
-            if (idx != -1) {
-                count++;
-                idx += pattern.length();
-            }
-        }
-        return count;
+    
+    /**
+     * Find the data name within the provided data string. Assume data is of the 
+     * correct format.
+     * @param data
+     * @return - data name or "" if data name is not found.
+     */
+    private String findDataName(String data) {
+        int colonIdx = data.indexOf(":");
+        String dataName = data.substring(0,colonIdx);
+        return dataName;
+    }
+    
+    
+    /**
+     * Find the angle data within the provided data string. Assume data is of 
+     * the correct format.
+     * @param data
+     * @return - String containing a comma separated list of angles.
+     */
+    private String findAngleString(String data) {
+        int colonIdx = data.indexOf(":");
+        String angles = data.substring(colonIdx+1);
+        return angles;
     }
 }
