a929f6866a697785d376f08628092f79ea29e2c0
Ranul Pallemulle <ranulp96@gmail.com>
2019-07-24 01:11:34 +0100

    Updated pizero builds. Renamed setup-macos to setup.py.

--- RaspiCode/resources/ipcamera/setup.py
+++ RaspiCode/resources/ipcamera/setup.py
@@ -1,20 +1,17 @@
 from setuptools import setup, Extension
+from pkgconfig import pkgconfig
 
 ipcamera_module = Extension('ipcamera',
-                            include_dirs =
-                            ['/usr/include/gstreamer-1.0','/usr/include/glib-2.0','/usr/lib/arm-linux-gnueabihf/glib-2.0/include'],
-                            libraries =
-                            ['pthread','gstreamer-1.0','gobject-2.0','glib-2.0'],
                             extra_compile_args=['-std=c++11'],
                             sources=['ipcamera_source.cpp','ipcamera_pymodule.cpp'],
-                            language='c++')
+                            language='c++',
+                            **pkgconfig('gstreamer-1.0','gstreamer-plugins-base-1.0'))
 
 redirect_module = Extension('streamredirect',
-                            include_dirs = ['/usr/include/gstreamer-1.0','/usr/include/glib-2.0','/usr/lib/arm-linux-gnueabihf/glib-2.0/include'],
-                            libraries = ['pthread','gstreamer-1.0','gobject-2.0','glib-2.0'],
                             extra_compile_args=['-std=c++11'],
                             sources=['redirect_source.cpp','redirect_pymodule.cpp'],
-                            language='c++')
+                            language='c++',
+                            **pkgconfig('gstreamer-1.0','gstreamer-plugins-base-1.0'))
 
 setup (name = 'IPCamera',
        version = '1.0',
